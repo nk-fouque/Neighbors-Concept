1b43795657c5f6316953e6d62a026d7a6dcd0420
nfouque <nicolas.fouque@irisa.fr>
2019-07-17 09:50:21 +0200

    Finished moving basic main parts in separate static functions

--- src/implementation/Main.java
+++ src/implementation/Main.java
@@ -1,98 +1,55 @@
 package implementation;
 
-import implementation.utils.AlgorithmTimer;
 import implementation.utils.CollectionsModel;
 import implementation.utils.SingletonStopwatchCollection;
-import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.reasoner.ReasonerRegistry;
-import org.apache.jena.sparql.core.Var;
 import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
-    public static void myLogsLevels(String level) {
-        switch (level) {
-            case "verbous": {
-                Logger.getLogger("org.apache.jena").setLevel(Level.INFO);
-                Logger.getLogger("implementation.Partition").setLevel(Level.DEBUG);
-                Logger.getLogger("implementation.Cluster").setLevel(Level.DEBUG);
-                Logger.getLogger("implementation.utils").setLevel(Level.INFO);
-                Logger.getLogger("implementation.matchTrees.MatchTreeNode").setLevel(Level.TRACE);
-                break;
-            }
-            case "silent": {
-                Logger.getLogger("org.apache.jena").setLevel(Level.INFO);
-                Logger.getLogger("implementation.Partition").setLevel(Level.INFO);
-                Logger.getLogger("implementation.Cluster").setLevel(Level.INFO);
-                Logger.getLogger("implementation.utils").setLevel(Level.OFF);
-                Logger.getLogger("implementation.matchTrees.MatchTreeNode").setLevel(Level.OFF);
-            }
-        }
-    }
-
-    public static CollectionsModel loadModelFromFile(String filename,String format,boolean verbose) throws IOException{
-        Model md = ModelFactory.createDefaultModel();
-        md.read(new FileInputStream(filename),null,format);
-        if(verbose){
-            md.write(System.out,"TURTLE");
-        }
-
-        Model saturated = ModelFactory.createInfModel(ReasonerRegistry.getRDFSReasoner(), md);
-        if(verbose) {
-            saturated.write(System.out, "TURTLE");
-        }
-
-        CollectionsModel res = new CollectionsModel(md,saturated);
-        return res;
-    }
-
     public static void main(String[] args) throws IOException {
-        // Logger setup
+
+        //Jena setup
         BasicConfigurator.configure();
-        myLogsLevels("silent");
+
+        // Logger setup
+        NeighborsImplementation.myLogsLevels("silent");
 
         // Loading Model from file
         String filename = "/udd/nfouque/Documents/default_mondial.nt";
 //        String filename = "/udd/nfouque/Documents/royal.ttl";
-        CollectionsModel model = loadModelFromFile(filename,"TTL",false);
+        CollectionsModel model = NeighborsImplementation.loadModelFromFile(filename, "TTL", false);
 
         // Choose node
         String uriTarget = "http://www.semwebtech.org/mondial/10/country/PE/";
 //        String uriTarget = "http://example.org/royal/Charlotte";
 
-        Partition p = new Partition(model,uriTarget);
-        System.out.println(p.getClusters().get(0));
-        System.out.println("Printing graph" + p.getGraph());
+        // Preparing Partition
+        Partition p = new Partition(model, uriTarget);
+//        System.out.println(p.getClusters().get(0));
+//        System.out.println("Printing graph" + model);
 
+        // Preparing file export
         FileWriter writer = new FileWriter("/udd/nfouque/Documents/results.txt");
-        // Apply algorithm
-        SingletonStopwatchCollection.resume("Main");
 
         // Defining Signal Handler for anytime implementation
         AtomicBoolean cut = new AtomicBoolean(false);
-        SignalHandler handler = sig -> {
-            System.out.println("Captured " + sig.getName());
-            cut.set(true);
-        };
+        SignalHandler handler = NeighborsImplementation.interruptCutter(cut);
         Signal.handle(new Signal("INT"), handler);
 
+        // Starting main Stopwatch
+        SingletonStopwatchCollection.resume("Main");
+
         // Launching the algorithm
-        AlgorithmTimer.planTimeOut(cut,15);
+        NeighborsImplementation.planTimeOut(cut, 150);
         int algoRun = p.partitionAlgorithm(cut);
 
+        // Processing results
         switch (algoRun) {
             case 0: {
                 System.out.println(p.toString());
@@ -111,15 +68,16 @@ public class Main {
             case 2: {
                 System.out.println("Anytime algorithm cut");
                 p.cut();
-                try{
+                try {
                     String results = p.toString();
                     System.out.println(results);
                     writer.write(results);
-                } catch (OutOfMemoryError err){
+                } catch (OutOfMemoryError err) {
                     System.out.println("Could not recover results, allocate more heap size or use a timeout");
                 }
             }
         }
+
         writer.close();
 
         System.out.println(SingletonStopwatchCollection.getElapsedMilliseconds("Main"));
---
+++ src/implementation/NeighborsImplementation.java
@@ -0,0 +1,66 @@
+package implementation;
+
+import implementation.utils.CollectionsModel;
+import implementation.utils.TimeOut;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import sun.misc.SignalHandler;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class NeighborsImplementation {
+
+    public static void myLogsLevels(String level) {
+        switch (level) {
+            case "verbous": {
+                Logger.getLogger("org.apache.jena").setLevel(Level.INFO);
+                Logger.getLogger("implementation.Partition").setLevel(Level.DEBUG);
+                Logger.getLogger("implementation.Cluster").setLevel(Level.DEBUG);
+                Logger.getLogger("implementation.utils").setLevel(Level.INFO);
+                Logger.getLogger("implementation.matchTrees.MatchTreeNode").setLevel(Level.TRACE);
+                break;
+            }
+            case "silent": {
+                Logger.getLogger("org.apache.jena").setLevel(Level.INFO);
+                Logger.getLogger("implementation.Partition").setLevel(Level.INFO);
+                Logger.getLogger("implementation.Cluster").setLevel(Level.INFO);
+                Logger.getLogger("implementation.utils").setLevel(Level.OFF);
+                Logger.getLogger("implementation.matchTrees.MatchTreeNode").setLevel(Level.OFF);
+            }
+        }
+    }
+
+    public static CollectionsModel loadModelFromFile(String filename, String format, boolean verbose) throws IOException {
+        Model md = ModelFactory.createDefaultModel();
+        md.read(new FileInputStream(filename), null, format);
+        if (verbose) {
+            md.write(System.out, "TURTLE");
+        }
+
+        Model saturated = ModelFactory.createInfModel(ReasonerRegistry.getRDFSReasoner(), md);
+        if (verbose) {
+            saturated.write(System.out, "TURTLE");
+        }
+
+        CollectionsModel res = new CollectionsModel(md, saturated);
+        return res;
+    }
+
+    public static void planTimeOut(AtomicBoolean cut, int seconds){
+        Thread thread = new Thread(new TimeOut(cut, seconds));
+        thread.start();
+    }
+
+    public static SignalHandler interruptCutter(AtomicBoolean cut){
+        SignalHandler handler = sig -> {
+            System.out.println("Captured " + sig.getName());
+            cut.set(true);
+        };
+        return handler;
+    }
+}
