4ba9bc9c34a46ede52ee37b68f7eb734390ab3b9
Fouque Nicolas <nicolas.fouque@insa-rennes.fr>
2019-10-28 14:57:16 +0100

    Foolproofing the launch script

--- run_interface.sh
+++ run_interface.sh
@@ -1,2 +1,3 @@
-. ./config
-"$jdk"/bin/java -classpath "$jena"/lib/*:"$javafx"/lib/:./out/production/Similarity_Search --module-path "$javafx"/lib --add-modules=javafx.fxml,javafx.controls implementation.gui.NeighborsInterface
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+. "$SCRIPTPATH"/config
+"$jdk"/bin/java -classpath "$jena"/lib/*:"$javafx"/lib/:"$SCRIPTPATH"/out/production/Similarity_Search --module-path "$javafx"/lib --add-modules=javafx.fxml,javafx.controls implementation.gui.NeighborsInterface
